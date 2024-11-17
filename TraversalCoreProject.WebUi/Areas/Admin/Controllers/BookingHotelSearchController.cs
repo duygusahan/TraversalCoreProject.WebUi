70c2df27b51190d5b470ca25285ec9bf15e1f96a
duygusahan <103382151+duygusahan@users.noreply.github.com>
2024-11-18 00:53:43 +0300

    ...

---
+++ TraversalCoreProject.WebUi/Areas/Admin/Controllers/BookingHotelSearchController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProject.WebUi.Areas.Admin.Models;
+
+namespace TraversalCoreProject.WebUi.Areas.Admin.Controllers
+{
+    [AllowAnonymous]
+    [Area("Admin")]
+    public class BookingHotelSearchController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels?dest_id=-755070&search_type=CITY&arrival_date=2024-11-18&departure_date=2024-11-22&adults=16&children_age=0%2C17&room_qty=1&page_number=1&units=metric&temperature_unit=c&languagecode=en-us&currency_code=EUR"),
+                Headers =
+    {
+        { "x-rapidapi-key", "9c3e504516msh738f9b82a4d009fp120b33jsnb4185e72491e" },
+        { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingHotelSearchViewModel>(body);
+                return View(values.data.hotels);
+            }
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCityDestinationId(string p)
+        {
+           
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://booking-com15.p.rapidapi.com/api/v1/hotels/searchDestination?query={p}"),
+                Headers =
+    {
+        { "x-rapidapi-key", "9c3e504516msh738f9b82a4d009fp120b33jsnb4185e72491e" },
+        { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                
+                return View();
+            }
+        }
+    }
+}
